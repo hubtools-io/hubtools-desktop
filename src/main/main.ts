3975c65b717bec3e96291d02d50eb3e63a2d804c
Markie Arnold <markiearnold@gmail.com>
2023-01-29 17:29:09 -0600

    Add releases in github

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,7 +19,7 @@ import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
 // ---------------------------------------------
-const server = 'http://hubtools-deploy-lcwhfwlwx-hubtools-io.vercel.app';
+const server = 'https://hubtools-deploy-bu3x2ekdl-hubtools-io.vercel.app';
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
 
 autoUpdater.setFeedURL(feed);
